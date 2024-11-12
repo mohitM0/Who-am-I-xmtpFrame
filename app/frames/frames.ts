f9dcf0d11b4c1e227cd53431b0fe887138484c61
mohitM0 <mohit@m0.ventures>
2024-11-12 14:30:32 +0530

    Created a new demo xmtp supported frame

---
+++ app/frames/frames.ts
@@ -0,0 +1,27 @@
+import { openframes } from "frames.js/middleware";
+import { createFrames } from "frames.js/next";
+import { getXmtpFrameMessage, isXmtpFrameActionPayload } from "frames.js/xmtp";
+ 
+export const frames = createFrames({
+  // basePath must point to the route of initial frame
+  // in this case it will reside in app/frames/route.tsx therefore /frames
+  basePath: "/frames",
+  middleware: [
+    openframes({
+      clientProtocol: {
+        id: "xmtp",
+        version: "2024-02-09",
+      },
+      handler: {
+        isValidPayload: (body) => isXmtpFrameActionPayload(body),
+        getFrameMessage: async (body) => {
+          if (!isXmtpFrameActionPayload(body)) {
+            return undefined;
+          }
+ 
+          return getXmtpFrameMessage(body);
+        },
+      },
+    }),
+  ],
+});
